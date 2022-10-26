049ed30f302a365a48d25931c47f347be31c1511
Nouh Abboune <nabboune@e2r7p6.1337.ma>
2022-10-26 04:44:26 +0100

    Mandatory and Bonus Part

---
+++ main.c
@@ -0,0 +1,92 @@
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	//*************************************
+	//					ft_atoi.c
+	//*************************************
+	// printf("%d\n", ft_atoi("-922337203775808"));
+	// printf("%d", atoi("-922337203775808"));
+	//*************************************
+	//					ft_bzero.c
+	//*************************************
+	// int	p1[] = {1, 2, 3, 4};
+	// int	p2[] = {1, 2, 3, 4};
+	// ft_bzero(p1, 6);
+	// printf("%d", p1[2]);
+	// bzero(p2, 6);
+	// printf("%d", p2[2]);
+	//*************************************
+	//					ft_calloc.c
+	//*************************************
+	// int	*p1;
+	// int	*p2;
+	// p1 = ft_calloc(5, 2);
+	// p2 = calloc(5, 2);
+	// printf("%c\n", p1[2]);
+	// printf("%c", p2[2]);
+	//*************************************
+	//	ft_isalnum.c	ft_isalpha.c	ft_isascii.c	ft_isdigit.c	ft_isprint.c
+	//*************************************
+	// EZ PZ
+	//*************************************
+	//					ft_itoa.c
+	//*************************************
+	//printf("%s\n", ft_itoa(1542));
+	//*************************************
+	//					ft_lstadd_back_bonus.c
+	//*************************************
+	// t_list	*p;
+	// t_list	*n;
+	// p = ft_lstnew("Hello");
+	// p->next = ft_lstnew("Helloo");
+	// p->next->next = ft_lstnew("Hellooo");
+	// p->next->next->next = ft_lstnew("Helloooo");
+	// n = ft_lstnew("Hellooooo");
+	// ft_lstadd_back(&p, n);
+	// while (p)
+	// {
+	// 	printf("%s\n", p->content);
+	// 	p = p->next;
+	// }
+	//*************************************
+	//					ft_lstadd_front_bonus.c
+	//*************************************
+	// t_list	*p;
+	// t_list	*n;
+	// p = ft_lstnew("Hello");
+	// p->next = ft_lstnew("Helloo");
+	// p->next->next = ft_lstnew("Hellooo");
+	// p->next->next->next = ft_lstnew("Helloooo");
+	// n = ft_lstnew("Hellooooo");
+	// ft_lstadd_front(&p, n);
+	// while (p)
+	// {
+	// 	printf("%s\n", p->content);
+	// 	p = p->next;
+	// }
+	//*************************************
+	//					ft_lstclear_bonus.c
+	//*************************************
+	// void	del(void *content)
+	// {
+	// 	content = 0;
+	// }
+	// t_list	*p;
+	// p = ft_lstnew("Hello");
+	// p->next = ft_lstnew("Helloo");
+	// p->next->next = ft_lstnew("Hellooo");
+	// p->next->next->next = ft_lstnew("Helloooo");
+	// ft_lstclear(&p, del);
+	// printf("%p", p);
+	//*************************************
+	//					ft_lstdelone_bonus.c
+	//*************************************
+	//*************************************
+	//					ft_strlcat.c
+	//*************************************
+	// printf("%zu", ft_strlcat("", "", 2));
+	// printf("%zu", strlcat("", "", 2));
+}
